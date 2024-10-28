16fef55e8eaaf5b1548cb7b83341a4c3fd7f284d
QiHuang02 <2830447227@qq.com>
2024-10-29 00:16:17 +0800

    添加Friend页面、将导航栏中的链接移动到Friend页面中

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -25,47 +25,48 @@ export const navbar = defineNavbarConfig([
       }
     ]
   },
-  {
-    text: '相关链接',
-    icon: '',
-    items: [
-      {
-        text: '我的世界相关',
-        icon: 'https://www.minecraft.net/etc.clientlibs/minecraftnet/clientlibs/clientlib-site/resources/favicon.ico',
-        items: [
-          {
-            text: '我的世界中文百科',
-            icon: 'https://zh.minecraft.wiki/images/Wiki.png?21467',
-            link: 'https://zh.minecraft.wiki/'
-          },
-          {
-            text: 'NeoForged',
-            icon: 'https://neoforged.net/favicon.ico',
-            link: 'https://neoforged.net/'
-          },
-          {
-            text: 'KubeJS 官方 Wiki',
-            icon: 'https://kubejs.com/logo_48.png',
-            link: 'https://kubejs.com/'
-          },
-          {
-            text: 'MC百科',
-            icon: 'https://www.mcmod.cn/images/favicon.ico',
-            link: 'https://www.mcmod.cn/'
-          }
-        ]
-      },
-      {
-        text: '实用网站',
-        icon: 'fluent:virtual-network-toolbox-20-filled',
-        items: [
-          {
-            text: 'Sci-hub',
-            icon: 'https://img.gufenxueshu.net/favicon.ico',
-            link: 'https://gfsoso.99lb.net/sci-hub.html'
-          }
-        ]
-      }
-    ]
-  }
+  { text: '联结', link: '/Friends.md'},
+  // {
+  //   text: '相关链接',
+  //   icon: '',
+  //   items: [
+  //     {
+  //       text: '我的世界相关',
+  //       icon: 'https://www.minecraft.net/etc.clientlibs/minecraftnet/clientlibs/clientlib-site/resources/favicon.ico',
+  //       items: [
+  //         {
+  //           text: '我的世界中文百科',
+  //           icon: 'https://zh.minecraft.wiki/images/Wiki.png?21467',
+  //           link: 'https://zh.minecraft.wiki/'
+  //         },
+  //         {
+  //           text: 'NeoForged',
+  //           icon: 'https://neoforged.net/favicon.ico',
+  //           link: 'https://neoforged.net/'
+  //         },
+  //         {
+  //           text: 'KubeJS 官方 Wiki',
+  //           icon: 'https://kubejs.com/logo_48.png',
+  //           link: 'https://kubejs.com/'
+  //         },
+  //         {
+  //           text: 'MC百科',
+  //           icon: 'https://www.mcmod.cn/images/favicon.ico',
+  //           link: 'https://www.mcmod.cn/'
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       text: '实用网站',
+  //       icon: 'fluent:virtual-network-toolbox-20-filled',
+  //       items: [
+  //         {
+  //           text: 'Sci-hub',
+  //           icon: 'https://img.gufenxueshu.net/favicon.ico',
+  //           link: 'https://gfsoso.99lb.net/sci-hub.html'
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // }
 ])
---
+++ docs/Friends.md
@@ -0,0 +1,44 @@
+---
+pageLayout: friends
+title: 联结
+createTime: 2024/10/28 23:47:21
+permalink: /Friends/
+groups:
+  - 
+    title: 友情链接
+    list:
+      -
+        name: CryChic 文档
+        link: https://docs.mihono.cn/zh/
+        avatar: https://docs.mihono.cn/svg/crychic.svg
+        desc: 一个由Crychic团队维护的我的世界文档.
+      -
+        name: 孤梦的KubeJS教程-1.20.1
+        link: https://gumeng.gitbook.io/kubejs-jiao-cheng-1.20.1
+        avatar: https://www.github.com/Gu-meng.png
+        desc: 这是1.20.1的kubejs内容分享，包括一些教程和项目.
+      -
+        name: PrizOwO'Blog
+        link: https://blogs.prizowo.net/
+        avatar: https://blogs.prizowo.net/icon/web.png
+        desc: 这里收集了各种 Minecraft 模组的教程、资源以及使用工具.
+  - 
+    title: 我的世界相关
+    list:
+      -
+        name: 我的世界 Wiki
+        link: https://zh.minecraft.wiki/
+        avatar: https://zh.minecraft.wiki/images/Wiki.png?21467
+      -
+        name: NeoForged
+        link: https://neoforged.net/
+        avatar: https://neoforged.net/favicon.ico
+      -
+        name: KubeJS
+        link: https://kubejs.com/
+        avatar: https://kubejs.com/logo_48.png
+      -
+        name: MC百科
+        link: https://www.mcmod.cn/
+        avatar: https://www.mcmod.cn/images/favicon.ico
+---
